823f576c2c68efe60b3ed3b29c4c97700e6af708
Wlad-ENAC <wladduma@gmail.com>
2016-01-22 10:11:01 +0100

    connection with ivy and mouse events catched

---
+++ src/Controller.java
@@ -0,0 +1,14 @@
+/**
+ * Created by rooty on 22/01/2016.
+ */
+public class Controller {
+
+    private Stroke stroke;
+
+    public Controller() {
+
+        this.stroke = new Stroke();
+    }
+
+
+}
