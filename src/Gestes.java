03de53c904241dfb5ac214279b97ae50cd821a6d
Wlad-ENAC <wladduma@gmail.com>
2016-02-05 11:03:50 +0100

    added gests

---
+++ src/Gestes.java
@@ -0,0 +1,145 @@
+import fr.dgac.ivy.IvyException;
+
+import java.awt.geom.Point2D;
+import java.io.*;
+import java.util.Locale;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+/**
+ * Created by rooty on 05/02/2016.
+ * Class to for gesture recognitions.
+ */
+public class Gestes {
+    Stroke templateSupprimer = new Stroke();
+    Stroke templateRectangle = new Stroke();
+    Stroke templateEllipse = new Stroke();
+    Stroke templateDeplacer = new Stroke();
+
+    public Gestes() {
+        setTemplate();
+    }
+
+    /**
+     * Set up the template (List of points).
+     */
+    public void setTemplate () {
+        templateSupprimer = lireFichier("templateSupprimer");
+        templateRectangle = lireFichier("templateRectangle");
+        templateEllipse = lireFichier("templateEllipse");
+        templateDeplacer = lireFichier("templateDeplacer");
+    }
+
+    /**
+     * This function can read the nomFic file.
+     * @param nomFic name file.
+     * @return the stroke.
+     */
+    public Stroke lireFichier(String nomFic) {
+        Stroke template = new Stroke();
+        try {
+            File f = new File (nomFic);
+            Scanner scanner = new Scanner (f);
+            scanner.useLocale(Locale.US);
+            scanner.useDelimiter(", |Point2D.Double\\[|\\]Point2D.Double\\[");
+            Double ptX;
+            Double ptY;
+            while (true) {
+                try {
+                    ptX = scanner.nextDouble();
+                    ptY = scanner.nextDouble();
+                    Point2D.Double point = new Point2D.Double(ptX,ptY);
+                    template.listePoint.add(point);
+
+                }
+                catch (NoSuchElementException exception) {
+                    break;
+                }
+            }
+            scanner.close();
+        }
+        catch (FileNotFoundException exception) {
+            System.out.println ("Le fichier n'a pas été trouvé");
+        }
+        return template;
+    }
+
+    public void ecrireFichier(String nomFic, String texte) {
+        //on va chercher le chemin et le nom du fichier et on me tout ca dans un String
+        String adressedufichier = System.getProperty("user.dir") + "/"+ nomFic;
+
+        //on met try si jamais il y a une exception
+        try {
+            /**
+             * BufferedWriter a besoin d un FileWriter,
+             * les 2 vont ensemble, on donne comme argument le nom du fichier
+             * true signifie qu on ajoute dans le fichier (append), on ne marque pas par dessus
+
+             */
+            FileWriter fw = new FileWriter(adressedufichier, true);
+
+            // le BufferedWriter output auquel on donne comme argument le FileWriter fw cree juste au dessus
+            BufferedWriter output = new BufferedWriter(fw);
+
+            //on marque dans le fichier ou plutot dans le BufferedWriter qui sert comme un tampon(stream)
+            output.write(texte);
+            //on peut utiliser plusieurs fois methode write
+
+            output.flush();
+            //ensuite flush envoie dans le fichier, ne pas oublier cette methode pour le BufferedWriter
+
+            output.close();
+            //et on le ferme
+            System.out.println("fichier créé");
+        }
+        catch(IOException ioe) {
+            System.out.print("Erreur : ");
+            ioe.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Determine what gestier has been made.
+     * @param s
+     * @return the chosen action. (!) default TODO Supprimer
+     * @throws IvyException
+     */
+    public int determinerStroke(Stroke s) throws IvyException {
+        // TODO put default Deplacement XXX
+        Double d1 = calculerDistance(s, templateSupprimer);
+        Double d2 = calculerDistance(s, templateRectangle);
+        if (d2 < d1) {
+            return 2;
+        }
+        Double d3 = calculerDistance(s, templateEllipse);
+        if ((d3 < d2) && (d3 < d1)) {
+            return 3;
+        }
+        Double d4 = calculerDistance(s, templateDeplacer);
+        if ((d4 < d1) && (d4 < d2) && (d4 < d3)) {
+            return 4;
+        }
+        return 1;
+    }
+
+    private Double calculerDistance(Stroke s1, Stroke s2){
+        Double distance = 0.0;
+        int i;
+        Double x1;
+        Double x2;
+        Double y1;
+        Double y2;
+        Double ajout;
+        for (i=0;i<s1.listePoint.size()-1;i++){
+            x1 = s1.listePoint.get(i).getX();
+            x2 = s2.listePoint.get(i).getX();
+            y1 = s1.listePoint.get(i).getY();
+            y2 = s2.listePoint.get(i).getY();
+            ajout = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+            distance += ajout;
+        }
+        return distance;
+    }
+
+}
--- src/IvyPaletteAgent.java
+++ src/IvyPaletteAgent.java
@@ -22,10 +22,7 @@ public class IvyPaletteAgent {
     private  Action action;
     private String lastReleaseX;
     private String lastReleaseY;
-    Stroke templateSupprimer = new Stroke();
-    Stroke templateRectangle = new Stroke();
-    Stroke templateEllipse = new Stroke();
-    Stroke templateDeplacer = new Stroke();
+    private Gestes gestes; // TODO delete
 
 
     public enum Action {
@@ -38,6 +35,7 @@ public class IvyPaletteAgent {
         stroke.init();
         bus = new Ivy("IvyPaletteAgent", "IvyPaletteAgent Ready", null);
         prete();
+        gestes = new Gestes(); // TODO in controller
         mousePressed();
         mouseReleased();
         mouseDragged();
@@ -153,9 +151,9 @@ public class IvyPaletteAgent {
         Double distance = 0.0;
         System.out.println("Analysing stroke ...." );
         stroke.normalize();
-        setTemplate();
+        gestes. setTemplate();
        // afficher(stroke);
-        determinerStroke(stroke);
+        determinerStrokeGests();
 
     }
 
@@ -168,25 +166,9 @@ public class IvyPaletteAgent {
     }
 
     // TODO PUT IN Controller
-    private void determinerStroke(Stroke s) throws IvyException {
-        // XXX simplify code
-        int formeReconnue = 1;
-        Double d1 = calculerDistance(stroke, templateSupprimer);
-        Double d2 = calculerDistance(stroke, templateRectangle);
-        if (d2 < d1){
-            formeReconnue = 2;
-        }
-        Double d3 = calculerDistance(stroke, templateEllipse);
-        if ((d3 < d2) &&(d3 < d1)){
-            formeReconnue = 3;
-        }
-        Double d4 = calculerDistance(stroke, templateDeplacer);
-        if ((d4<d1)&&(d4<d2)&&(d4<d3)){
-            formeReconnue = 4;
-        }
+    private void determinerStrokeGests() throws IvyException {
 
-
-        switch (formeReconnue){
+        switch (gestes.determinerStroke(stroke)){
             case 1 : System.out.println("Supprimer");
                 action = Action.DELETE;
                 break;
@@ -239,114 +221,6 @@ public class IvyPaletteAgent {
         }
     }
 
-    // TODO PUT IN Controller
-    private Double calculerDistance(Stroke s1, Stroke s2){
-        Double distance = 0.0;
-        int i;
-        Double x1;
-        Double x2;
-        Double y1;
-        Double y2;
-        Double ajout;
-        for (i=0;i<s1.listePoint.size()-1;i++){
-            x1 = s1.listePoint.get(i).getX();
-            x2 = s2.listePoint.get(i).getX();
-            y1 = s1.listePoint.get(i).getY();
-            y2 = s2.listePoint.get(i).getY();
-            ajout = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-            distance += ajout;
-        }
-        return distance;
-    }
-
-    // TODO PUT IN Reconnaissance de geste
-    /**
-     * Set up the template (List of points).
-     */
-    private void setTemplate () {
-        templateSupprimer = lireFichier("templateSupprimer");
-        templateRectangle = lireFichier("templateRectangle");
-        templateEllipse = lireFichier("templateEllipse");
-        templateDeplacer = lireFichier("templateDeplacer");
-    }
-
-    // TODO PUT IN Controller
-    private Stroke lireFichier(String nomFic){
-        Stroke template = new Stroke();
-        try
-        {
-
-            File f = new File (nomFic);
-            Scanner scanner = new Scanner (f);
-
-            scanner.useLocale(Locale.US);
-            scanner.useDelimiter(", |Point2D.Double\\[|\\]Point2D.Double\\[");
-            Double ptX;
-            Double ptY;
-
-            while (true)
-            {
-                try
-                {
-                    ptX = scanner.nextDouble();
-                    ptY = scanner.nextDouble();
-                    Point2D.Double point = new Point2D.Double(ptX,ptY);
-                    template.listePoint.add(point);
-
-                }
-                catch (NoSuchElementException exception)
-                {
-                    break;
-                }
-            }
-
-            scanner.close();
-        }
-        catch (FileNotFoundException exception)
-        {
-            System.out.println ("Le fichier n'a pas été trouvé");
-        }
-        return template;
-    }
-
-    // TODO PUT IN Reconnaissance de geste
-    public void ecrireFichier(String nomFic, String texte)
-    {
-        //on va chercher le chemin et le nom du fichier et on me tout ca dans un String
-        String adressedufichier = System.getProperty("user.dir") + "/"+ nomFic;
-
-        //on met try si jamais il y a une exception
-        try
-        {
-            /**
-             * BufferedWriter a besoin d un FileWriter,
-             * les 2 vont ensemble, on donne comme argument le nom du fichier
-             * true signifie qu on ajoute dans le fichier (append), on ne marque pas par dessus
-
-             */
-            FileWriter fw = new FileWriter(adressedufichier, true);
-
-            // le BufferedWriter output auquel on donne comme argument le FileWriter fw cree juste au dessus
-            BufferedWriter output = new BufferedWriter(fw);
-
-            //on marque dans le fichier ou plutot dans le BufferedWriter qui sert comme un tampon(stream)
-            output.write(texte);
-            //on peut utiliser plusieurs fois methode write
-
-            output.flush();
-            //ensuite flush envoie dans le fichier, ne pas oublier cette methode pour le BufferedWriter
-
-            output.close();
-            //et on le ferme
-            System.out.println("fichier créé");
-        }
-        catch(IOException ioe){
-            System.out.print("Erreur : ");
-            ioe.printStackTrace();
-        }
-
-    }
-
     private void testRectangle () {
         try {
             bus.sendMsg("Palette:CreerRectangle x=90");
@@ -360,7 +234,5 @@ public class IvyPaletteAgent {
             System.out.println("dfgdhdfhgdxfhgdfhxdf");
             e.printStackTrace();
         }
-
     }
-
 }
