87e5b3fbfc7e09d6e76a3e065797fec149e1aa70
Wlad-ENAC <wladduma@gmail.com>
2016-02-11 14:53:45 +0100

    full grammard

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,8 @@ import java.awt.geom.Point2D;
 public class Main {
 
     public static void main(String [] args) throws IvyException {
-        //Audio audio = new Audio();
         IvyPaletteAgent iv = new IvyPaletteAgent();
-//        AffichageTemplate affichageTemplate = new AffichageTemplate();
-//        JFrame affichageFrame = new JFrame();
-//        affichageFrame.add(affichageTemplate);
-//        affichageFrame.setVisible(true);
+        Audio audio = new Audio();
     }
 
 }
